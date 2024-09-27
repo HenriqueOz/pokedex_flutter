f32cec8cc2ad2a0d5c58e9c986b40ce12fc81820
Peguinm <pedroozorio30@gmail.com>
2024-09-27 20:43:47 -0300

    added pre cached images in evolution tab and forms tab

--- lib/app/modules/pokemon/widgets/bottom_part/tabs/tab_evolution.dart
+++ lib/app/modules/pokemon/widgets/bottom_part/tabs/tab_evolution.dart
@@ -1,10 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex_app/app/core/formatter/formatter.dart';
 import 'package:pokedex_app/app/core/ui/custom_theme.dart';
 import 'package:pokedex_app/app/models/pokemon_model.dart';
 import 'package:pokedex_app/app/modules/pokemon/widgets/bottom_part/pokemon_variations_list.dart';
 
-class TabEvolution extends StatelessWidget {
+class TabEvolution extends StatefulWidget {
   final List<PokemonModel> list;
   final PokemonModel mainModel;
   final Color primaryColor;
@@ -12,28 +13,42 @@ class TabEvolution extends StatelessWidget {
   const TabEvolution({super.key, required this.mainModel, required this.list, required this.primaryColor});
 
   @override
+  State<TabEvolution> createState() => _TabEvolutionState();
+}
+
+class _TabEvolutionState extends State<TabEvolution> {
+  @override
+  void didChangeDependencies() {
+    for (var element in widget.list) {
+      precacheImage(CachedNetworkImageProvider(element.imageUrl), context);
+      precacheImage(CachedNetworkImageProvider(element.shinyImageUrl), context);
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return list.isNotEmpty
+    return widget.list.isNotEmpty
         ? Padding(
             padding: const EdgeInsets.symmetric(vertical: 40),
             child: ListView.builder(
               primary: false,
               shrinkWrap: true,
-              itemCount: list.length,
+              itemCount: widget.list.length,
               itemBuilder: (context, index) {
-                final model = list[index];
+                final model = widget.list[index];
 
-                return PokemonVariationsList(mainModel: mainModel, model: model);
+                return PokemonVariationsList(mainModel: widget.mainModel, model: model);
               },
             ),
           )
         : Padding(
             padding: const EdgeInsets.symmetric(vertical: 30),
             child: Text(
-              "${Formatter.captalize(text: mainModel.name)} doesn't have evolutions",
+              "${Formatter.captalize(text: widget.mainModel.name)} doesn't have evolutions",
               textAlign: TextAlign.center,
               style: CustomTheme.pokedexLabels.copyWith(
-                color: mainModel.primaryColor!,
+                color: widget.mainModel.primaryColor!,
               ),
             ),
           );
