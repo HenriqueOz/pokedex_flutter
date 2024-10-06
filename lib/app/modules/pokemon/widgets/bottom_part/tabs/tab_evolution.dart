376e44ecfba130efbd7796a63d2555abc81ff508
Peguinm <pedroozorio30@gmail.com>
2024-10-06 15:45:38 -0300

    before revert to 2dd84ad

--- lib/app/modules/pokemon/widgets/bottom_part/tabs/tab_evolution.dart
+++ lib/app/modules/pokemon/widgets/bottom_part/tabs/tab_evolution.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex_app/app/core/formatter/formatter.dart';
 import 'package:pokedex_app/app/core/ui/custom_theme.dart';
